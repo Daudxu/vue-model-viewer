6c79bcd64fbf8be2c3c427bddbb8ba456019c16e
Daudxu <875126243@qq.com>
2022-02-25 12:02:41 +0000

    Deploying to gh-pages from  @ 3ddb3837b75b7c697ad760e71b50e16b2560f7c8 🚀

--- packages/vueModelViewer/index.js
+++
@@ -1,9 +0,0 @@
-// 导入组件，组件必须声明 name
-import VueModelViewer from './src/main.vue'
-
-// 为组件添加 install 方法，用于按需引入
-VueModelViewer.install = function (Vue) {
-  Vue.component(VueModelViewer.name, VueModelViewer)
-}
-
-export default VueModelViewer
