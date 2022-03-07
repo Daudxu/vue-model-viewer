c58fc6b11c48e4ab9e1d0f4b25aa28b7ca0fae53
xudan <xudan@apowo.com>
2022-03-07 12:45:44 +0800

    Test demo

--- examples/main.js
+++ examples/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 // import VueModelViewer from '../packages/index'
 // import VueModelViewer from '../index.js'
-import VueModelViewer from '../lib/vue-model-viewer.umd.js'
-// import VueModelViewer from 'vue-model-viewer'
+// import VueModelViewer from '../lib/vue-model-viewer.umd.js'
+import VueModelViewer from 'vue-model-viewer'
 Vue.config.productionTip = false
 Vue.use(VueModelViewer);
 new Vue({
