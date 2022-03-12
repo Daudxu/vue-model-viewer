48c4f4f4a95a2c68d20a8b38bc66a90688fcc1ae
xudan <xudan@apowo.com>
2022-03-13 00:09:50 +0800

    fix /model-viewer

--- examples/main.js
+++ examples/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import VueModelViewer from '../packages/index'
+import VueModelViewer from '../packages/index'
 // import VueModelViewer from '../index.js'
 // import VueModelViewer from '../lib/vue-model-viewer.umd.js'
-import VueModelViewer from 'vue-model-viewer'
+// import VueModelViewer from 'vue-model-viewer'
 Vue.config.productionTip = false
 Vue.use(VueModelViewer);
 new Vue({
